3734f8bac937662c4426c834d36c15aa02524136
kaylaobritsch <kaylaobritsch@MacBook-Pro.local>
2017-05-12 15:34:25 -0400

    Followed Tutorial

---
+++ js/contacts.js
@@ -0,0 +1,20 @@
+var peeps = [
+  {
+    name: 'Mercury Man',
+    email: 'theman@mercury.com',
+    tel: '+05551234',
+    loc: [56001200, 69816900]
+  },
+  {
+    name: 'Venus ‘Venturer',
+    email: 'venus@theadventurer.com',
+    tel: '+15551234',
+    loc: [1074770000, 108939000]
+  },
+  {
+    name: 'Jupiter Juggernaut',
+    email: 'thejugger@jupiter.com',
+    tel: '+55551234',
+    loc: [816040000, 740550000]
+  },
+];
